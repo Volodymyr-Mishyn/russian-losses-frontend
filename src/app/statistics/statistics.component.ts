c66a1f63826b2bc6a56daa3bd24ba37d9ddb3695
Volodymyr Mishyn <pti.mishin@gmail.com>
2023-11-13 19:18:01 +0200

    -added material and tailwind

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatSidenavModule],
   templateUrl: './statistics.component.html',
   styleUrl: './statistics.component.scss',
 })
