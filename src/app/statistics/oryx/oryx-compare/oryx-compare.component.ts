2a3341d8bc9032d9a8d225fa415cf018f88ab4e9
Volodymyr Mishyn <pti.mishin@gmail.com>
2023-11-30 17:01:27 +0200

    -oryx routing components stubs

---
+++ src/app/statistics/oryx/oryx-compare/oryx-compare.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-oryx-compare',
+  standalone: true,
+  imports: [
+    CommonModule,
+  ],
+  templateUrl: './oryx-compare.component.html',
+  styleUrl: './oryx-compare.component.scss',
+})
+export class OryxCompareComponent { }
---
+++ src/app/statistics/oryx/oryx-country-losses/oryx-country-losses.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-oryx-country-losses',
+  standalone: true,
+  imports: [
+    CommonModule,
+  ],
+  templateUrl: './oryx-country-losses.component.html',
+  styleUrl: './oryx-country-losses.component.scss',
+})
+export class OryxCountryLossesComponent { }
