66397616770ee484dd428f3e25e839c8890a3bf2
Volodymyr Mishyn <pti.mishin@gmail.com>
2023-12-12 16:51:15 +0200

    -displaying data for comparison of oryx entities

--- src/app/statistics/oryx/oryx-compare/oryx-compare.component.ts
+++ src/app/statistics/oryx/oryx-compare/oryx-compare.component.ts
@@ -5,11 +5,12 @@ import { selectOryxComparison } from '../../_store/selectors/oryx.selectors';
 import { OryxCompareTypeComponent } from './components/oryx-compare-type/oryx-compare-type.component';
 import { Observable } from 'rxjs';
 import { OryxComparison } from '../../_models/data/oryx/oryx-comparison';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-oryx-compare',
   standalone: true,
-  imports: [CommonModule, OryxCompareTypeComponent],
+  imports: [CommonModule, OryxCompareTypeComponent, MatDividerModule],
   templateUrl: './oryx-compare.component.html',
   styleUrl: './oryx-compare.component.scss',
 })
