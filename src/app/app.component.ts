a05a3a70824c98359cc17354db69f54f9c03d22d
Volodymyr Mishyn <pti.mishin@gmail.com>
2023-12-20 19:34:45 +0200

    some first localization attempts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,9 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  title = $localize`:@@pageTitle:Data and infographic about russian invasion of Ukraine`;
+  constructor(private _titleService: Title) {
+    this._titleService.setTitle(this.title);
+  }
+}
