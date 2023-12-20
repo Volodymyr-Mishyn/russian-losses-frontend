093a1aa06f376f7e3add0d4140820f3accdf559d
Volodymyr Mishyn <pti.mishin@gmail.com>
2023-12-20 02:41:28 +0200

    -scroll to top

--- src/app/statistics/oryx/oryx.component.ts
+++ src/app/statistics/oryx/oryx.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { ScrollToTopComponent } from '../components/scroll-to-top/scroll-to-top.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    ScrollToTopComponent,
+  ],
   templateUrl: './oryx.component.html',
   styleUrl: './oryx.component.scss',
 })
-export class OryxComponent {}
+export class OryxComponent implements OnInit {
+  @ViewChild('scrollDiv', { read: ElementRef, static: false })
+  public scrollContainer!: ElementRef;
+  public containerReady = false;
+
+  public ngOnInit(): void {
+    setTimeout(() => {
+      this.containerReady = true;
+    });
+  }
+}
