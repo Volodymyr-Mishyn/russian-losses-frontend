ed6a32e48fbe5e473f677cff4503f4cd66bae1ed
Volodymyr Mishyn <pti.mishin@gmail.com>
2023-12-23 13:30:35 +0200

    outstanding visual improvements

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -12,12 +12,22 @@ import { ToggleThemeComponent } from '../components/toggle-theme/toggle-theme.co
 
 const NAVIGATION: Array<NavigationElement> = [
   {
-    title: 'mod',
-    tooltip: 'Ministry of defense',
-    route: '/statistics/ministry-of-defense',
+    title: $localize`MoD`,
+    tooltip: $localize`Ministry of defense of Ukraine data`,
+    icon: 'assets/img/mod.svg',
+    route: `/statistics/ministry-of-defense`,
   },
-  { title: 'oryx', tooltip: 'Oryx data', route: '/statistics/oryx' },
-  { title: 'about', tooltip: 'About', route: '/statistics/about' },
+  {
+    title: $localize`Oryx`,
+    tooltip: $localize`Oryx OSINT data`,
+    icon: 'assets/img/oryx.svg',
+    route: `/statistics/oryx`,
+  },
+  {
+    title: $localize`About`,
+    tooltip: $localize`About`,
+    route: `/statistics/about`,
+  },
 ];
 
 @Component({
