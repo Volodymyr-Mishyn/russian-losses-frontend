e4cc4b19122f1f914964f2588a57c689cdf6e5bc
Volodymyr Mishyn <pti.mishin@gmail.com>
2023-12-26 11:54:47 +0200

    additional translation for oryx

--- src/app/statistics/oryx/oryx-country-losses/components/oryx-side-losses-presenter/oryx-side-losses-presenter.component.ts
+++ src/app/statistics/oryx/oryx-country-losses/components/oryx-side-losses-presenter/oryx-side-losses-presenter.component.ts
@@ -5,6 +5,7 @@ import { OryxRouteMetadata } from '../../../_models/oryx-route-metadata';
 import { OryxSideLosses } from '../../../../_models/data/oryx/oryx-model';
 import { OryxTypeLossesComponent } from '../oryx-type-losses/oryx-type-losses.component';
 import { OryxStatisticsComponent } from '../oryx-statistics/oryx-statistics.component';
+import { TranslatePipe } from '../../../../../pipes/translate.pipe';
 
 @Component({
   selector: 'app-oryx-side-losses-presenter',
@@ -15,6 +16,7 @@ import { OryxStatisticsComponent } from '../oryx-statistics/oryx-statistics.comp
     OryxTypeLossesComponent,
     OryxStatisticsComponent,
   ],
+  providers: [TranslatePipe],
   templateUrl: './oryx-side-losses-presenter.component.html',
   styleUrl: './oryx-side-losses-presenter.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -22,7 +24,17 @@ import { OryxStatisticsComponent } from '../oryx-statistics/oryx-statistics.comp
 export class OryxSideLossesPresenterComponent {
   @Input()
   public metadata!: OryxRouteMetadata;
+  public sideName!: string;
+  private _data!: OryxSideLosses;
 
   @Input()
-  public data!: OryxSideLosses;
+  public get data(): OryxSideLosses {
+    return this._data;
+  }
+
+  public set data(value: OryxSideLosses) {
+    this._data = value;
+    this.sideName = this._translatePipe.transform(this._data.countryName);
+  }
+  constructor(private _translatePipe: TranslatePipe) {}
 }
