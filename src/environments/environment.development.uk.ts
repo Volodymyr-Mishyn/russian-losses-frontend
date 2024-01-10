fb493353343b08cc4cb04200e1e6c3804d7ca61e
Volodymyr Mishyn <pti.mishin@gmail.com>
2024-01-11 01:38:45 +0200

    env files configured to work with remote service

--- src/environments/environment.development.uk.ts
+++ src/environments/environment.development.uk.ts
@@ -1,5 +1,7 @@
 export const environment = {
-  backendUrl: 'http://localhost:3000',
+  // backendUrl: 'http://localhost:3000',
+  backendUrl:
+    'http://russian-losses-load-balancer-1002635241.eu-central-1.elb.amazonaws.com',
   apiUrl: '/api',
   production: false,
   locale: 'uk',
