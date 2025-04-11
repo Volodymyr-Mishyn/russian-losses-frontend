46993a2875d81cc1fdf29cff9bfcc4b4f9e69fad
Volodymyr Mishyn <pti.mishin@gmail.com>
2025-04-11 20:52:33 +0300

    attempt with separate builds

--- src/environments/environment.development.uk.ts
+++ src/environments/environment.development.uk.ts
@@ -1,6 +1,6 @@
 export const environment = {
   apiUrl: '/api',
-  serverApiUrl: 'http://backend:8080', // used during SSR
+  serverApiUrl: 'http://localhost:8080', // used during SSR
   production: false,
   deployUrl: 'https://russian-losses.in.ua',
 };
