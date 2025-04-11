884a30a81d2f70be682c77b4e528628338cdcb6d
Volodymyr Mishyn <pti.mishin@gmail.com>
2025-04-11 16:28:18 +0300

    -migration to VPS instead of AWS fargate

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,4 @@
 export const environment = {
-  backendUrl: 'https://russian-losses.in.ua',
   apiUrl: '/api',
   production: false,
   deployUrl: 'https://russian-losses.in.ua',
--- src/environments/environment.uk.ts
+++ src/environments/environment.uk.ts
@@ -1,5 +1,4 @@
 export const environment = {
-  backendUrl: 'https://russian-losses.in.ua',
   apiUrl: '/api',
   production: true,
   deployUrl: 'https://russian-losses.in.ua',
